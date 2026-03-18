e490c97e1259b89238ddaa989e2d9d206f2a8f04
Karsten Flache <karsten.flache@plus.aok.de>
2026-03-18 10:39:17 +0100

    fix(ui): GeneAggression in Statistik-Anzeige ergänzen
    avgGenes() gibt jetzt 4 Werte zurück; drawStats() zeigt ØAgg neben ØEff

--- ui/hud.go
+++ ui/hud.go
@@ -102,7 +102,7 @@ func (h *HUD) drawSidebar(screen *ebiten.Image, snap *sim.WorldSnapshot, cfg con
 }
 
 func (h *HUD) drawStats(screen *ebiten.Image, tx, ty int, snap *sim.WorldSnapshot) int {
-	avgSpeed, avgSight, avgEff := avgGenes(snap.Individuals)
+	avgSpeed, avgSight, avgEff, avgAggr := avgGenes(snap.Individuals)
 	lines := []string{
 		"Statistik:",
 		fmt.Sprintf("Tick:  %d", snap.Tick),
@@ -110,7 +110,7 @@ func (h *HUD) drawStats(screen *ebiten.Image, tx, ty int, snap *sim.WorldSnapsho
 		fmt.Sprintf("Geb: %d  Tod: %d", snap.Stats.Births, snap.Stats.Deaths),
 		fmt.Sprintf("Räuber: %d  Kills: %d", snap.Stats.Predators, snap.Stats.Kills),
 		fmt.Sprintf("ØSpd:%.2f ØSgt:%.2f", avgSpeed, avgSight),
-		fmt.Sprintf("ØEff:%.2f", avgEff),
+		fmt.Sprintf("ØEff:%.2f ØAgg:%.2f", avgEff, avgAggr),
 	}
 	for _, l := range lines {
 		ebitenutil.DebugPrintAt(screen, l, tx, ty)
@@ -325,7 +325,7 @@ func (h *HUD) drawChartLegend(screen *ebiten.Image, pad, ty int) {
 	}
 }
 
-func avgGenes(inds []entity.Individual) (speed, sight, eff float32) {
+func avgGenes(inds []entity.Individual) (speed, sight, eff, aggression float32) {
 	if len(inds) == 0 {
 		return
 	}
@@ -333,7 +333,8 @@ func avgGenes(inds []entity.Individual) (speed, sight, eff float32) {
 		speed += ind.Genes[entity.GeneSpeed]
 		sight += ind.Genes[entity.GeneSight]
 		eff += ind.Genes[entity.GeneEfficiency]
+		aggression += ind.Genes[entity.GeneAggression]
 	}
 	n := float32(len(inds))
-	return speed / n, sight / n, eff / n
+	return speed / n, sight / n, eff / n, aggression / n
 }
